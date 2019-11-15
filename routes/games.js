d78dea0db8ef5cd0e9c536016b19d24756ced6dd
Sombra González <gonzalez@puzzle.ch>
2019-11-16 00:43:46 +0100

    Load game and save fairplayed

--- routes/games.js
+++ routes/games.js
@@ -4,29 +4,29 @@ var db = require('./config/database');
 
 /* GET players listing. */
 router.get('/', function(req, res, next) {
-  db.many('SELECT * from player')
+  db.many('SELECT * from game')
     .then(function(data) {
-      res.render('games', { players: 'test' });
+      res.render('games', { games: data });
     })
     .catch(function(error) {
       console.log('ERROR:', error);
     });
 });
 
-/* GET player by id*/
+/* GET game by id*/
 router.get('/:id', function(req, res, next) {
-  db.one('SELECT * from player where id=$1', req.params.id)
+  db.one('SELECT * from game where id=$1', req.params.id)
     .then(function(data) {
-      res.render('games', { player: 'test' });
+      res.render('game', { game: data });
     })
     .catch(function(error) {
       console.log('ERROR:', error);
     });
 });
 
-router.post('/:id/fairest', function(req, res) {
-  console.log('Fair play for :', req.params.id);
-  res.end('yes');
+router.post('/:gameId/team/:teamId/fairplayed', function(req, res) {
+  const fairplayfeedback = db.one('INSERT INTO game_team_feedback (team_id, game_id) VALUES($1, $2) RETURNING id', [req.params.teamId, req.params.gameId]);
+  res.end("Fairplay saved", fairplayfeedback);
 });
 
 module.exports = router;
